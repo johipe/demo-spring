6f99ee0a36325b2d21411a4764e11e6abe0472f5
Johanna <johipe@gmail.com>
2019-10-22 20:02:13 -0400

    primer comit

---
+++ src/main/java/ar/com/taller/spring/demo/DemoApplication.java
@@ -0,0 +1,8 @@
+package ar.com.taller.spring.demo;
+
+public class DemoApplication {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/ar/com/taller/spring/demo/DemoRest.java
@@ -0,0 +1,15 @@
+package ar.com.taller.spring.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRest {
+
+    @RequestMapping(value = "/hola{nombre}", method = RequestMethod.GET)
+    public String holamundo(@PathVariable String nombre){
+        return "hola mundo";
+    }
+}
